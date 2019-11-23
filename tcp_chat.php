ba4fb8a30ea704b2caa5786d6336b00237598ad3
dyoshikawa <dyoshikawa1993@gmail.com>
2019-11-24 02:49:56 +0900

    fix

--- tcp_chat.php
+++ tcp_chat.php
@@ -2,21 +2,48 @@
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-socket_bind($socket, '0.0.0.0', 8000);
+try {
+    socket_bind($socket, '0.0.0.0', 8000);
 
-socket_listen($socket, 1);
+    socket_listen($socket, 5);
 
-echo 'start...';
-while ($remote = socket_accept($socket)) {
-    $red = socket_read($remote, 10000);
+    echo 'start...';
+    $remotes = [];
+    echo '1人目を待っています。' . PHP_EOL;
+    array_push($remotes, socket_accept($socket));
+    echo '1人目が入りました。' . PHP_EOL;
+    echo '2人目を待っています。' . PHP_EOL;
+    array_push($remotes, socket_accept($socket));
+    echo '2人目が入りました。' . PHP_EOL;
+    echo 'チャット開始' . PHP_EOL;
 
-    socket_write($remote, $red);
-}
+    $thread = new class($remotes) extends Thread
+    {
+        public $remotes;
 
-socket_close($socket);
+        public function __construct($remotes)
+        {
+            $this->remotes = $remotes;
+        }
 
-class MyThread extends Thread {
-    public function run () {
-        sleep(5);
+        public function run()
+        {
+            while ($buffer = socket_read($this->remotes[0], 1000)) {
+                socket_write($this->remotes[1], $buffer);
+            }
+        }
+    };
+
+    $thread->start();
+
+    while ($buffer = socket_read($remotes[1], 1000)) {
+        socket_write($remotes[0], $buffer);
     }
+
+} finally {
+    $thread->join();
+    foreach ($remotes as $remote) {
+        socket_close($socket);
+    }
+    socket_close($socket);
 }
--- tcp_echo.php
+++ tcp_echo.php
@@ -4,14 +4,18 @@ $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
 socket_bind($socket, '0.0.0.0', 8000);
 
-socket_listen($socket, 1);
+socket_listen($socket, 3);
 
 echo 'start...';
+$remotes = [];
 while ($remote = socket_accept($socket)) {
-    $red = socket_read($remote, 10000);
-    echo 'log: ' . $red;
-
-    socket_write($remote, $red);
+    array_push($remotes, $remote);
+    while ($buffer = socket_read($remote, 10000)) {
+//        socket_write($remote, $buffer);
+        foreach ($remotes as $r) {
+            socket_write($r, $buffer);
+        }
+    }
 }
 
 socket_close($socket);
