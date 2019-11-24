06a66e48706fdecec80d4dca5f7728cec3cc5974
dyoshikawa <dyoshikawa1993@gmail.com>
2019-11-24 11:10:27 +0900

    fix

--- http.php
+++ http.php
@@ -6,19 +6,26 @@ socket_bind($socket, '0.0.0.0', 8000);
 
 socket_listen($socket, 1);
 
-echo 'start...';
+echo 'start...' . PHP_EOL;
 $remote = socket_accept($socket);
 
 // 本当はループで$bufferを順次読み取るべき
 $red = socket_read($remote, 10000);
-
-echo 'log: ' . $red;
+myLogger($red);
 
 socket_write($remote, 'HTTP/1.0 200 OK' . PHP_EOL);
 socket_write($remote, 'Content-Type: text/plain' . PHP_EOL);
 socket_write($remote, PHP_EOL);
-socket_write($remote, 'Hello');
+socket_write($remote, 'Hello' . PHP_EOL);
 
 socket_close($remote);
 
 socket_close($socket);
+
+function myLogger(string $str)
+{
+    $explodeds = explode(PHP_EOL, $str);
+    foreach ($explodeds as $exploded) {
+        echo 'log: ' . $exploded . PHP_EOL;
+    }
+}
--- tcp_echo.php
+++ tcp_echo.php
@@ -7,15 +7,11 @@ socket_bind($socket, '0.0.0.0', 8000);
 socket_listen($socket, 3);
 
 echo 'start...';
-$remotes = [];
 while ($remote = socket_accept($socket)) {
-    array_push($remotes, $remote);
     while ($buffer = socket_read($remote, 10000)) {
-//        socket_write($remote, $buffer);
-        foreach ($remotes as $r) {
-            socket_write($r, $buffer);
-        }
+        socket_write($remote, $buffer);
     }
+    socket_close($remote);
 }
 
 socket_close($socket);
